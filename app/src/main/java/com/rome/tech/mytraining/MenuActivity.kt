b8f852f970acc9493e3a1bba231357921d267351
Walter Romero <95034089+waldrom68@users.noreply.github.com>
2023-09-06 12:34:01 -0300

    Start TODO app

--- app/src/main/java/com/rome/tech/mytraining/MenuActivity.kt
+++ app/src/main/java/com/rome/tech/mytraining/MenuActivity.kt
@@ -6,23 +6,54 @@ import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.rome.tech.mytraining.IMCapp.ImcMainActivity
+import com.rome.tech.mytraining.todo_app.TodoMainActivity
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var btnIMCApp: Button
+    private lateinit var btnOtherApp: Button
+    private lateinit var btnTodoApp: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+        initElements()
+        initListeners()
+
+        initUI()
+
+    }
+
+    private fun initElements() {
         var btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+        var btnTodoApp =  findViewById<Button>(R.id.btnTODO)
+
         var btnOtherApp = findViewById<Button>(R.id.btnOther)
 
+    }
+
+    private fun initListeners() {
         btnIMCApp.setOnClickListener { navigateToIMCApp() }
+        btnIMCApp.setOnClickListener { navigateToTODOApp() }
+
         btnOtherApp.setOnClickListener { Log.i("wal", "Click en btnOtherApp") }
 
     }
 
+
+
+    private fun initUI() {
+        TODO("Not yet implemented")
+    }
+
     private fun navigateToIMCApp() {
         var intent = Intent(this, ImcMainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToTODOApp() {
+        var intent = Intent(this, TodoMainActivity::class.java)
         startActivity(intent)
     }
 
---
+++ app/src/main/java/com/rome/tech/mytraining/todo_app/TodoMainActivity.kt
@@ -0,0 +1,12 @@
+package com.rome.tech.mytraining.todo_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rome.tech.mytraining.R
+
+class TodoMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo_main)
+    }
+}
