c0559f091258f8be3ce444fac4b88c7e6cecad39
Walter Romero <95034089+waldrom68@users.noreply.github.com>
2023-09-12 12:52:25 -0300

    UI: basic layout - main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,12 +30,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        viewBinding = true
-        // dataBinding = true
+//    buildFeatures {
+//        viewBinding = true
+//        // dataBinding = true
+//    }
+    viewBinding {
+        enable = true
     }
 }
 
--- app/src/main/java/com/rome/tech/mytraining/superheros_app/SuperHerosListActivity.kt
+++ app/src/main/java/com/rome/tech/mytraining/superheros_app/SuperHerosListActivity.kt
@@ -1,12 +1,42 @@
 package com.rome.tech.mytraining.superheros_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rome.tech.mytraining.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import com.rome.tech.mytraining.databinding.ActivitySuperHerosListBinding
 
 class SuperHerosListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySuperHerosListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySuperHerosListBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_super_heros_list)
+//        setContentView(R.layout.activity_super_heros_list)
+        setContentView(binding.root)
+
+        initUI()
+
     }
+
+    private fun initUI() {
+        binding.herosSearchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchByName(query.orEmpty())
+
+                return false
+
+            }
+
+            override fun onQueryTextChange(newText: String?) = false
+
+        })
+
+    }
+
+    private fun searchByName(query: String) {
+        Log.i("busqueda", "Click en submit ${query.trim()}-")
+    }
+
 }
